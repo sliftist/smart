52c13033a4a586935482c19a226f88bcca8f5159
sliftist <sliftist@gmail.com>
2025-11-02 14:58:10 -0500

    File name

--- startup.sh
+++ startup.sh
@@ -4,11 +4,23 @@ sleep 15
 PATH=/usr/local/bin:/usr/bin:/bin:$PATH
 export PATH
 
-killall screen
-sleep 1
+if ! screen -ls | grep -q "\.watch"; then
+  screen -dmS watch
+  sleep 0.5
+fi
+# No kill, so it doesn't kill itself
+screen -S watch -X stuff 'cd ~/smart && yarn watch\n'
 
-screen -dmS thermostat
+if ! screen -ls | grep -q "\.thermostat"; then
+  screen -dmS thermostat
+  sleep 0.5
+fi
+screen -S thermostat -X stuff '^C^C^C\n'
 screen -S thermostat -X stuff 'cd ~/smart && yarn thermostat\n'
 
-screen -dmS humid
+if ! screen -ls | grep -q "\.humid"; then
+  screen -dmS humid
+  sleep 0.5
+fi
+screen -S humid -X stuff '^C^C^C\n'
 screen -S humid -X stuff 'cd ~/smart && yarn humid\n'
