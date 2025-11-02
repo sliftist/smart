bef39db28fd40cc5c9da93129588e1abaeda0c58
sliftist <sliftist@gmail.com>
2025-11-02 15:07:59 -0500

    Less wet

--- startup.sh
+++ startup.sh
@@ -7,9 +7,9 @@ export PATH
 if ! screen -ls | grep -q "\.watch"; then
   screen -dmS watch
   sleep 0.5
+  screen -S watch -X stuff 'cd ~/smart && yarn watch\n'
 fi
 # No kill, so it doesn't kill itself
-screen -S watch -X stuff 'cd ~/smart && yarn watch\n'
 
 if ! screen -ls | grep -q "\.thermostat"; then
   screen -dmS thermostat
@@ -21,6 +21,6 @@ screen -S thermostat -X stuff 'cd ~/smart && yarn thermostat\n'
 if ! screen -ls | grep -q "\.humid"; then
   screen -dmS humid
   sleep 0.5
+  # killing breaks rtl
+  screen -S humid -X stuff 'cd ~/smart && yarn humid\n'
 fi
-screen -S humid -X stuff '^C^C^C\n'
-screen -S humid -X stuff 'cd ~/smart && yarn humid\n'
