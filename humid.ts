e7641c6ac226da9bd621ce8ce4fc382e4d025937
sliftist <sliftist@gmail.com>
2025-12-16 16:50:06 -0500

    Colder

--- humid.ts
+++ humid.ts
@@ -45,11 +45,11 @@ const SUPER_COOLING_TEMPERATURE_THRESHOLD = 1.5;
 
 let TEMPERATURE_PLAN = [
     // Cold, so we go to sleep
-    { time: 0, temperature: 24 },
+    { time: 0, temperature: 23 },
     // Warm, to wake up
-    { time: 9.5, temperature: 27 },
+    { time: 9.5, temperature: 26 },
     // Less warm, as our computer will start to get hot around this time
-    { time: 12.5, temperature: 25 },
+    { time: 12.5, temperature: 24 },
 ];
 
 
