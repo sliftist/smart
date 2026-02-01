ff1765b78627a12a2e1d409853dc9e3db7a484a0
sliftist <sliftist@gmail.com>
2026-02-01 16:54:20 -0500

    Cool down

--- humid.ts
+++ humid.ts
@@ -53,7 +53,7 @@ let TEMPERATURE_PLAN = [
     // Warm, to wake up
     { time: 9.5, temperature: 25 },
     // Less warm, as our computer will start to get hot around this time
-    { time: 12.5, temperature: 23 },
+    { time: 12.5, temperature: 22 },
 ];
 
 
