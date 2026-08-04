50020670f60de32522240fe67b96c0ab0c62a175
sliftist <sliftist@gmail.com>
2026-08-04 15:05:12 -0400

    Change

--- humid.ts
+++ humid.ts
@@ -57,9 +57,9 @@ const SUPER_COOLING_TEMPERATURE_THRESHOLD = 100;
 
 // TODO: Adjust the temperature targets slightly based on outside temperatures.
 let TEMPERATURE_PLAN = [
-    { time: 0, temperature: 23 },
-    { time: 9.5, temperature: 23 },
-    { time: 12.5, temperature: 23 },
+    { time: 0, temperature: 22 },
+    { time: 9.5, temperature: 22 },
+    { time: 12.5, temperature: 22 },
 ];
 
 
